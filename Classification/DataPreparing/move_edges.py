99c125de6f40ca12f06778f489ba0715d78ca8f6
Marim1611 <marimnaser813@gmail.com>
2023-06-23 12:33:53 +0300

    test edges split

---
+++ Classification/DataPreparing/move_edges.py
@@ -0,0 +1,43 @@
+import sys, os, random
+import numpy as np
+import shutil
+'''
+this script map the edges files to the test nodes files
+
+** How to run?
+    python move_edges.py  <test nodes path> <edges path> <test edges path>
+
+
+test nodes path: abs path of the folder containing the test nodes files
+edges path: abs path of the folder containing the edges files
+test edges path: abs path of the folder to save the test edges files in (if not exists, it will be created)
+
+'''
+test_nodes_path=sys.argv[1]
+edges_path=sys.argv[2]
+test_edges_path=sys.argv[3]
+
+test_nodes_files=[]
+for file in os.listdir(test_nodes_path):
+    #store file name in list
+    file=file[:-5]
+    test_nodes_files.append(file[5:])
+
+# create the new path if not exists
+if not os.path.exists(test_edges_path):
+    os.mkdir(test_edges_path) 
+    
+for file in os.listdir(edges_path):
+    # remove extension from file name
+    file_temp=file[:-4]
+    file_temp=file_temp[6:]
+    #check if file is in test_nodes_files
+    if file_temp in test_nodes_files:
+        
+        #move file to test_edges_path
+        new_path = test_edges_path+'/' + file
+        old_path = edges_path+'/' + file
+        shutil.move(old_path, new_path)
+
+
+
