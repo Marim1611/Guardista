f08ae2725848f95b53c32763595704ff1676646c
Marim1611 <marimnaser813@gmail.com>
2023-06-25 14:53:44 +0300

    UI initial config

---
+++ UI/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <h1>HELLO IT'S GUARDISTA</h1>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
