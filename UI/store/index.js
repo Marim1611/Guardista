72c6746344d7d416e15db85a85c3cc49f641ef48
Mariem Muhammed <76264155+mariemzayn18@users.noreply.github.com>
2023-06-26 13:06:03 +0300

    report template

--- UI/store/index.js
+++ UI/store/index.js
@@ -1,13 +1,12 @@
 export const state = newFunction()
 function newFunction() {
   return () => ({
-    Theme: 
-      {
-        color1: '#9d0000', // RED
-        color2: '#545454', // Gray
-        titleFontSize: '38px',
-        subTitleFontSize: '24px',
-        paragraphFontSize: '20px',
-      }
-  })
+    Theme: {
+      color1: "#9d0000", // RED
+      color2: "#9d0000", // Gray
+      titleFontSize: "38px",
+      subTitleFontSize: "24px",
+      paragraphFontSize: "20px",
+    },
+  });
 }
