43f97c2f85729e2d1b19068c706dc0cd8aaf32a3
Marim1611 <marimnaser813@gmail.com>
2023-06-30 18:42:52 +0300

    cookies pacakage added

--- UI/nuxt.config.js
+++ UI/nuxt.config.js
@@ -33,7 +33,9 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    'cookie-universal-nuxt'
+  ],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
